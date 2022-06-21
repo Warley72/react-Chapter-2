7d55b30dc00b0097bdb698103077cff50939cf96
carlos warley <carloswarley0@gmail.com>
2022-06-20 23:11:39 -0300

    summry

--- dtmoney/src/style/global.ts
+++ dtmoney/src/style/global.ts
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;
         --red: #E52E4D;
-        --green #33cc95;
+        --green: #33cc95;
         --blue: #5429CC;
         
         --blue-light: #6933FF;
