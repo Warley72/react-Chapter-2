cb7c20d74823b64b0e5b1e710a7994abed80435c
carlos warley <carloswarley0@gmail.com>
2022-06-20 23:58:31 -0300

    table

---
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+    return(
+        <Container>
+            <table>
+               <thead>
+                <tr>
+                    <th>Titulo</th>
+                    <th>Valor</th>
+                    <th>Categoria</th>
+                    <th>Data</th>
+                </tr>
+                </thead> 
+
+                <tbody>
+                    <tr>
+                        <td>salario</td>
+                        <td className="deposit">5000.00</td>
+                        <td>Trabalho</td>
+                        <td>20/06/2022</td>
+                    </tr>
+
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">-1100.00</td>
+                        <td>Casa</td>
+                        <td>25/06/2022</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </Container>
+    );
+}
